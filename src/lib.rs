f479bf1daffc64e9d2e095999cd002f47d0bf884
lyric228 <xhikzerox@gmail.com>
2025-06-15 22:35:20 +0500

    better project structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,9 @@
-pub mod io {
-    pub mod cmd;
-    pub mod env;
-    pub mod log;
-}
-pub mod net {
-    pub mod ipv4;
-    pub mod ipv6;
-}
-pub mod math {
-    pub mod bin;
-    pub mod hex;
-}
-pub mod utils {
-    pub mod ascii;
-    pub mod rand;
-    pub mod term;
-}
-pub mod types {
-    pub mod checker;
-    pub mod error;
-}
-pub mod time {
-    pub mod sleep;
-    pub use sleep::*;
-}
+pub mod io;
+pub mod net;
+pub mod math;
+pub mod utils;
+pub mod types;
+pub mod time;
 
 pub use io::*;
 pub use types::error::*;
---
+++ src/utils/mod.rs
@@ -0,0 +1,3 @@
+pub mod ascii;
+pub mod rand;
+pub mod term;
