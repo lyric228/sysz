da87d41e6c2a64e15bb179cb9c674f73107c159c
lyric228 <xhikzerox@gmail.com>
2025-06-18 03:35:49 +0500

    ipv4 a& ipv6 refactoring

--- src/net/ipv6.rs
+++ src/net/ipv6.rs
@@ -1,19 +1,17 @@
 use std::net::{Ipv6Addr, SocketAddrV6};
 
 /// Checks if a string is a valid IPv6 address with a specified port.
-pub fn is_valid_ipv6(s: &str) -> bool {
+pub fn is_valid(s: &str) -> bool {
     s.parse::<SocketAddrV6>().is_ok()
 }
 
 /// Converts a string to a SocketAddrV6 if it's a valid IPv6 address with a port.
-/// Returns `Some(SocketAddrV6)` on success, `None` otherwise.
-pub fn str_to_ipv6(s: &str) -> Option<SocketAddrV6> {
+pub fn convert(s: &str) -> Option<SocketAddrV6> {
     s.parse::<SocketAddrV6>().ok()
 }
 
 /// Creates a SocketAddrV6 from an IP address string, port, flow info, and scope ID.
-/// Returns `Some(SocketAddrV6)` on success, `None` if the IP string is invalid.
-pub fn create_ipv6_socket(
+pub fn create_socket(
     ip: &str,
     port: u16,
     flowinfo: u32,
