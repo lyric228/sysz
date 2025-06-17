7f00151db7be488419b354b8798de2998bd2c1a1
lyric228 <xhikzerox@gmail.com>
2025-06-18 03:55:46 +0500

    reffactoring

--- src/io/env.rs
+++ src/io/env.rs
@@ -1,12 +1,12 @@
 use std::env;
 
 /// Returns a vector of command-line arguments, including the program name.
-pub fn get_full_args() -> Vec<String> {
+pub fn full_args() -> Vec<String> {
     env::args().collect()
 }
 
 /// Returns a vector of command-line arguments, excluding the program name.
-pub fn get_args() -> Vec<String> {
+pub fn args() -> Vec<String> {
     let mut args: Vec<String> = env::args().collect();
     if !args.is_empty() {
         args.remove(0);
