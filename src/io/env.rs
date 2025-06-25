61dafe75852fcf5dc4c64fcdfa6e47dcd01f9650
lyric228 <xhikzerox@gmail.com>
2025-06-25 23:06:45 +0500

    env inline

--- src/io/env.rs
+++ src/io/env.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 /// Returns a vector of command-line arguments, including the program name.
+#[inline]
 pub fn full_args() -> Vec<String> {
     env::args().collect()
 }
@@ -8,8 +9,10 @@ pub fn full_args() -> Vec<String> {
 /// Returns a vector of command-line arguments, excluding the program name.
 pub fn args() -> Vec<String> {
     let mut args: Vec<String> = env::args().collect();
+
     if !args.is_empty() {
         args.remove(0);
     }
+
     args
 }
