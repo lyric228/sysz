2233741b3c3507a3dbea94d46c6af5b451582a0f
lyric228 <xhikzerox@gmail.com>
2025-06-25 23:09:43 +0500

    net inline

--- src/net/ipv4.rs
+++ src/net/ipv4.rs
@@ -1,16 +1,19 @@
 use std::net::{Ipv4Addr, SocketAddrV4};
 
 /// Checks if a string is a valid IPv4 address with a port.
+#[inline]
 pub fn is_valid(s: &str) -> bool {
     s.parse::<SocketAddrV4>().is_ok()
 }
 
 /// Converts a string to a SocketAddrV4 if it's a valid IPv4 address with a port.
+#[inline]
 pub fn convert(s: &str) -> Option<SocketAddrV4> {
     s.parse::<SocketAddrV4>().ok()
 }
 
 /// Creates a SocketAddrV4 from an IP address string and a port.
+#[inline]
 pub fn create_socket(ip: &str, port: u16) -> Option<SocketAddrV4> {
     let ip_addr = ip.parse::<Ipv4Addr>().ok()?;
     Some(SocketAddrV4::new(ip_addr, port))
--- src/net/ipv6.rs
+++ src/net/ipv6.rs
@@ -1,16 +1,19 @@
 use std::net::{Ipv6Addr, SocketAddrV6};
 
 /// Checks if a string is a valid IPv6 address with a specified port.
+#[inline]
 pub fn is_valid(s: &str) -> bool {
     s.parse::<SocketAddrV6>().is_ok()
 }
 
 /// Converts a string to a SocketAddrV6 if it's a valid IPv6 address with a port.
+#[inline]
 pub fn convert(s: &str) -> Option<SocketAddrV6> {
     s.parse::<SocketAddrV6>().ok()
 }
 
 /// Creates a SocketAddrV6 from an IP address string, port, flow info, and scope ID.
+#[inline]
 pub fn create_socket(ip: &str, port: u16, flowinfo: u32, scope_id: u32) -> Option<SocketAddrV6> {
     let ip_addr = ip.parse::<Ipv6Addr>().ok()?;
     Some(SocketAddrV6::new(ip_addr, port, flowinfo, scope_id))
